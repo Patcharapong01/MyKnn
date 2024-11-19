60bbba5bac4d7d511e88d12864968c47fd631744
Patcharapong Titivanich <gamemingkung3@gmail.com>
2024-11-19 14:53:21 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.title("hello")
-st.header("hello")
+st.title("hello word")
+st.header("Hello GG")
 
 st.image('./img/patcharapong.jpg')
 st.subheader("Patcharapong Titivanich")
 
 dt=pd.read_csv('./data/iris-3.csv')
-st.header("ข้อมูลส่วนตัว")
+st.header("ข้อมูลดอกไม้")
+st.write(dt.head(10))
